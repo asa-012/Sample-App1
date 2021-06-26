d27563c23d17d4cca3abb449d90f644b347d8d52
asa-012 <yjdyj.20356299@gmail.com>
2021-06-26 19:19:03 +0900

    add:navigation画面遷移追加、Activity textview削除

--- app/src/main/java/com/asa/atrae_sample_app1/main/MainFragment.kt
+++ app/src/main/java/com/asa/atrae_sample_app1/main/MainFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.asa.atrae_sample_app1.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -36,6 +39,15 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<Button>(R.id.button_example)
+        button.setOnClickListener{
+            findNavController().navigate(
+                R.id.action_mainFragment_to_detailFragment)
+        }
     }
 
     companion object {
