c1f43178006d45e8c260503d5aeef95beb8dfab5
asa-012 <yjdyj.20356299@gmail.com>
2021-07-05 19:42:33 +0900

    fix:interfaceのMainRepositoryを修正

--- app/src/main/java/com/asa/atrae_sample_app1/main/MainRepository.kt
+++ app/src/main/java/com/asa/atrae_sample_app1/main/MainRepository.kt
@@ -1,15 +1,9 @@
 package com.asa.atrae_sample_app1.main
 
-import androidx.lifecycle.LiveData
 import com.asa.atrae_sample_app1.network.RandomUsersDataPage
-import com.asa.atrae_sample_app1.network.UsersApi
-import com.asa.atrae_sample_app1.network.UsersProperties
 
 interface MainRepository {
 
-    val status:LiveData<UsersApiStatus>
-    val properties:LiveData<RandomUsersDataPage>
-
-    suspend fun getUsersProperties()
+    suspend fun getUsersProperties():RandomUsersDataPage
 
 }
